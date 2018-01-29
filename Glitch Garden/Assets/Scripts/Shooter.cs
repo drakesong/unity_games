4e117f507816a677cdd949671a342f9502bc45a1
Drake Song <drakesong216@gmail.com>
2018-01-28 16:42:56 -0800

    Defenders detect attackers

--- Glitch Garden/Assets/Scripts/Shooter.cs
+++ Glitch Garden/Assets/Scripts/Shooter.cs
@@ -6,13 +6,53 @@ public class Shooter : MonoBehaviour {
 	public GameObject projectile, gun;
 	
 	private GameObject projectileParent;
+	private Animator animator;
+	private Spawner myLaneSpawner;
 
 	void Start() {
+		animator = GetComponent<Animator>();
 		projectileParent = GameObject.Find("Projectiles");
 		
 		if (!projectileParent) {
 			projectileParent = new GameObject("Projectiles");
 		}
+		
+		SetMyLaneSpawner();
+	}
+	
+	void Update() {
+		if (IsAttackerAheadInLane()) {
+			animator.SetBool("isAttacking", true);
+		}
+		else {
+			animator.SetBool("isAttacking", false);
+		}
+	}
+	
+	void SetMyLaneSpawner() {
+		Spawner[] spawnerArray = GameObject.FindObjectsOfType<Spawner>();
+		
+		foreach (Spawner spawner in spawnerArray) {
+			if (spawner.transform.position.y == transform.position.y) {
+				myLaneSpawner = spawner;
+				return;
+			}
+		}
+		
+		Debug.LogError(name + " can't find spawner in lane");
+	}
+	
+	bool IsAttackerAheadInLane() {
+		if (myLaneSpawner.transform.childCount <= 0) {
+			return false;
+		}
+		foreach (Transform attacker in myLaneSpawner.transform) {
+			if (attacker.transform.position.x > transform.position.x) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	private void Fire() {
