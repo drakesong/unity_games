290367bcfb6bf7ac58e234bf542448e2bd0ce4a3
Drake Song <drakesong216@gmail.com>
2018-02-15 00:21:01 -0800

    Added helicopter

--- Zombie Runner/Assets/Scripts/Helicopter.cs
+++ Zombie Runner/Assets/Scripts/Helicopter.cs
@@ -6,16 +6,19 @@ public class Helicopter : MonoBehaviour {
     public AudioClip callSound;
 
     private bool called = false;
+    private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
-		
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    if (Input.GetButtonDown("CallHeli") && !called) {
             called = true;
+            audioSource.clip = callSound;
+            audioSource.Play();
         }	
 	}
 }
