4e6598c93c83f21f6bf2f1710e271483f78b0a41
Drake Song <drakesong216@gmail.com>
2018-02-15 01:39:28 -0800

    Added inner voice

--- Zombie Runner/Assets/Scripts/Player.cs
+++ Zombie Runner/Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public Transform playerSpawnPoints;
-    private bool respawn = false;
     public Helicopter helicopter;
+    public AudioClip whatHappened;
 
+    private bool respawn = false;
     private Transform[] spawnPoints;
     private bool lastToggle = false;
+    private AudioSource innerVoice;
 
     // Use this for initialization
     void Start() {
         spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        foreach (AudioSource audioSource in audioSources) {
+            if (audioSource.priority == 1) {
+                innerVoice = audioSource;
+            }
+        }
+        innerVoice.clip = whatHappened;
+        innerVoice.Play();
     }
 
     // Update is called once per frame
